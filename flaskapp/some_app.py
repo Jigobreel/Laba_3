b5e5fea544472b68d3ac283a43d91e84a4547f69
Jigobreel <117265695+Jigobreel@users.noreply.github.com>
2023-01-11 14:08:03 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -10,3 +10,13 @@ def hello():
 
 if __name__ == "__main__":
   app.run(host='127.0.0.1',port=5000)
+
+from flask import render_template
+@app.route("/data_to")
+def data_to():
+  #make value with data for translating in template
+  some_pars = {'user':'Ivan','color':'red'}
+  some_str = 'Hello my dear friends!'
+  some_value = 10
+  #translate data in template and run it
+  return render_template('simple.html',some_str=some_str,some_value=some_value,some_pars=some_pars)

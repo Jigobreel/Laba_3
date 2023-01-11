ebbb1c36c2fbda2fff4273fe8be55aa99beb3b33
Jigobreel <117265695+Jigobreel@users.noreply.github.com>
2023-01-11 16:15:08 +0700

    Update st.sh

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,10 +1,8 @@
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
 sleep 5
 echo start client
-python client.py
-APP_CODE=$?
-sleep 25
+python3 client.py
+sleep 5
 echo $APP_PID
 kill -TERM $APP_PID
-echo app code $APP_CODE
 exit 0
